cb9e0f987ca29258e713db6759bb0aa9037b537a
kukkich <vitia.iezhovqqqq@gmail.com>
2024-10-02 10:00:42 +0700

    Исправил ошибки

---
+++ GridApp/PointsCollectionJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using SharpMath.FEM.Geometry;
+using SharpMath.Geometry._2D;
+
+namespace GridApp;
+
+public class PointsCollectionJsonConverter : JsonConverter<IPointsCollection<Point2D>>
+{
+    public override IPointsCollection<Point2D> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, IPointsCollection<Point2D> value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+        for (int i = 0; i < value.TotalPoints; i++)
+        {
+            JsonSerializer.Serialize(writer, value[i], options);
+        }
+        writer.WriteEndArray();
+    }
+}
--- GridApp/Program.cs
+++ GridApp/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text.Json;
+using GridApp;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using SharpMath.FEM.Core;
 using SharpMath.FEM.Geometry._2D.Quad;
 using SharpMath.Geometry._2D;
 using SharpMath.Geometry.Splitting;
@@ -26,6 +29,45 @@ void ConfigureServices(IServiceCollection services)
 // ConfigureServices(services);
 // var provider = services.BuildServiceProvider();
 
+Grid<Point2D, IElement> TaskArea()
+{
+    List<int> xStepsCounts = [3, 5, 8, 5, 3];
+    List<int> yStepsCounts = [7, 4];
+   
+    var gridBuilder = new RegularGridBuilder();
+    var def = new RegularGridDefinition(
+        ControlPoints: new Point2D[,]
+        {
+            { new(1, 1), new (10, 1), new(14, 1), new(16, 1), new(20, 1), new(29, 1) },
+            { new(1, 12), new (10, 12), new(14, 10), new(16, 10), new(20, 12), new(29, 12) },
+            { new(1, 16), new (10, 16), new(14, 16), new(16, 16), new(20, 16), new(29, 16) },
+        },
+        xStepsCounts.Select(x => new UniformSplitter(x)).ToArray<ICurveSplitter>(),
+        yStepsCounts.Select(x => new UniformSplitter(x)).ToArray<ICurveSplitter>(),
+        [
+            new AreaDefinition(0, 5, 0, 1),
+            new AreaDefinition(0, 1, 1, 2),
+            new AreaDefinition(1, 4, 1, 2),
+            new AreaDefinition(4, 5, 1, 2),
+        ], 
+        []);
+
+    var grid = gridBuilder.Build(def);
+    return grid;
+}
+
+string SerializeGrid(Grid<Point2D, IElement> grid)
+{
+    var json = JsonSerializer.Serialize(grid, new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Converters = { new PointsCollectionJsonConverter() }
+    });
+
+    Console.WriteLine(json);
+    return json;
+}
+
 var gridBuilder = new RegularGridBuilder();
 var def = new RegularGridDefinition(
     ControlPoints: new Point2D[,]
@@ -35,11 +77,11 @@ var def = new RegularGridDefinition(
     },
     [new UniformSplitter(2)],
     [new UniformSplitter(2)],
-    [new AreaDefinition(0, 1, 0, 1)],
+    [new AreaDefinition(0, 1, 0, 1)], 
     []);
-var grid = gridBuilder.Build(def);
+
+var grid = TaskArea();
+
+var str = SerializeGrid(grid);
 
 Console.WriteLine();
-// var logger = provider.GetRequiredService<ILogger<Program>>();
-// logger.LogInformation("GridApp, You're just a miserable copy of me!");
-// logger.LogCritical("No, I'm the upgrade!");
