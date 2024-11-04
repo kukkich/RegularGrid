378850577754799542446cce27f3152246586785
kukkich <vitia.iezhovqqqq@gmail.com>
2024-11-04 13:15:04 +0700

    Поддержка отрицательного коэффициента разрядки

--- GridApp/Program.cs
+++ GridApp/Program.cs
@@ -17,39 +17,12 @@ void ConfigureServices(IServiceCollection services)
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
         .Build();
-    var section = configuration.GetSection("GridApp");
     services.AddSingleton<IConfiguration>(configuration.GetSection("GridApp"));
 
     services.AddSingleton<IGridDefinitionProvider<RegularGridDefinition>, GridDefinitionReader>();
     services.AddSingleton<RegularGridBuilder>();
 }
 
-Grid<Point2D, IElement> TaskArea()
-{
-    List<int> xStepsCounts = [3, 5, 8, 5, 3];
-    List<int> yStepsCounts = [7, 4];
-   
-    var gridBuilder = new RegularGridBuilder();
-    var def = new RegularGridDefinition(
-        ControlPoints: new Point2D[,]
-        {
-            { new(1, 1), new (10, 1), new(14, 1), new(16, 1), new(20, 1), new(29, 1) },
-            { new(1, 12), new (10, 12), new(14, 10), new(16, 10), new(20, 12), new(29, 12) },
-            { new(1, 16), new (10, 16), new(14, 16), new(16, 16), new(20, 16), new(29, 16) },
-        },
-        xStepsCounts.Select(x => new UniformSplitter(x)).ToArray<ICurveSplitter>(),
-        yStepsCounts.Select(x => new UniformSplitter(x)).ToArray<ICurveSplitter>(),
-        [
-            new AreaDefinition(0, 5, 0, 1),
-            new AreaDefinition(0, 1, 1, 2),
-            new AreaDefinition(1, 4, 1, 2),
-            new AreaDefinition(4, 5, 1, 2),
-        ], 
-        []);
-
-    var grid = gridBuilder.Build(def);
-    return grid;
-}
 
 string SerializeGrid(Grid<Point2D, IElement> grid)
 {
@@ -73,6 +46,6 @@ var gridBuilder = provider.GetRequiredService<RegularGridBuilder>();
 var gridDefinition = gridReader.Get();
 var grid = gridBuilder.Build(gridDefinition);
 
-var str = SerializeGrid(grid);
+SerializeGrid(grid);
 
 Console.WriteLine();
